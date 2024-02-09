c34668f0fb6b109fe295146888e6bf791f2e519c
ank809 <ankita51523@gmail.com>
2024-02-10 01:29:30 +0530

    user registration

--- login_app/urls.py
+++ login_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns=[
     path('login_page/', views.login_user, name="login_page"),
     path('home_page', views.home_page, name='home_page'),
-    path('logout/', views.logout_user, name='logout')
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register_user')
 ]
--- login_app/views.py
+++ login_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import authenticate,  login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def home_page(request):
     if request.user.is_authenticated:
@@ -27,3 +28,17 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return redirect('home_page')
+
+def register_user(request):
+    if request.method=='POST':
+        form= UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username= form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            user=authenticate(request, username=username, password=password)
+            login(request, user=user)
+            return redirect('home_page')
+    else:
+        form=UserCreationForm()
+    return render(request, 'register_user.html', {"form":form})
