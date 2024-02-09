520760971440b7cbcd40d8882b8dda11771270d3
ank809 <ankita51523@gmail.com>
2024-02-10 00:34:32 +0530

    logout functionality added

--- login_app/urls.py
+++ login_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('login_page/', views.login_user, name="login_page"),
-    path('home_page', views.home_page, name='home_page')
+    path('home_page', views.home_page, name='home_page'),
+    path('logout/', views.logout_user, name='logout')
 ]
--- login_app/views.py
+++ login_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect, HttpResponse
-from django.contrib.auth import authenticate,  login
+from django.contrib.auth import authenticate,  login, logout
 from django.contrib import messages
 
 def home_page(request):
-    return render(request, 'home_page.html')
+    if request.user.is_authenticated:
+        username = request.user.username
+        return render(request, 'home_page.html', {"username":username})
+    else:
+        return render(request, "home_page.html")
 
 def login_user(request):
     if request.method=='POST':
@@ -18,4 +22,8 @@ def login_user(request):
         
     else:
         return render(request, 'login_page.html')
+    
 
+def logout_user(request):
+    logout(request)
+    return redirect('home_page')
